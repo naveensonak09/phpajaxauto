5f6c75f65f8d421f2755aefef45f6c281b50a4b8
naveen kumar <naveen_kumar@excellencetechnologies.in>
2020-07-31 12:29:13 +0530

    auto refresh not working

--- js/autoajax.js
+++ js/autoajax.js
@@ -1,36 +1,39 @@
-function loadOrg() {
-          window.tbl.ajax.reload(null, false);
-      }
-    
-      $(document).ready(function () {
-          var tbl = $('#userTable').DataTable({
-              "bProcessing": false,
-              "serverSide": false,
-              "ajax": {
-                  url: 'server.php',
-                  type: "GET", 
-                  error: function () {  
-                      $("#userTable").css("display", "none");
-                  }
-              },
-              "columns": [
-                  {
-                      "data": null, "sortable": false,
-                      render: function (data, type, row, meta) {
-                          return meta.row + meta.settings._iDisplayStart + 1;
-                      }
-                  },
-                  { "data": "id" },
-                  { "data": "name" },
-                  { "data": "email" },
-                  { "data": "message" },
-                  { "data": "date" },
-              ],
-          });
-          window.tbl = tbl;
-      });
-      
-      setInterval(function() {
-        loadOrg();
-        console.log(1);
-      },3000);
+$(document).ready(function(){
+    $.ajax({
+        url: 'server.php',
+        type: 'get',
+        dataType: 'JSON',
+        success: function(response){
+            var len = response.length;
+            for(var i=0; i<len; i++){
+                var id = response[i].id;
+                var name = response[i].name;
+                var email = response[i].email;
+                var message = response[i].message;
+                var date = response[i].date;
+
+                var tr_str = "<tr>" +
+                    "<td align='center'>" + (i+1) + "</td>" +
+                    "<td align='center'>" + name + "</td>" +
+                    "<td align='center'>" + email + "</td>" +
+                    "<td align='center'>" + message + "</td>" +
+                    "<td align='center'>" + date + "</td>" +
+                    "</tr>";
+                $("#userTable tbody").append(tr_str);
+                
+                setInterval(function(){
+                    
+                    $("#userTable tbody").load(tr_str);
+                   
+                    console.log(1);
+                },6000);
+               
+            }
+
+           
+
+        }
+    });
+   
+});
+
--- server.php
+++ server.php
@@ -8,9 +8,18 @@
     $json_array = array();
     while($row=mysqli_fetch_assoc($data))
     {
-        $json_array[] = $row;
+        $id = $row['id'];
+        $name = $row['name'];
+        $email = $row['email'];
+        $message = $row['message'];
+        $date = $row['date'];
+
+        $json_array[] = array( "id" => $id,
+                                "name" => $name,
+                                "email" => $email,
+                                "message" => $message,
+                                "date" =>  $date);
     }
-        echo json_encode(['data'=>$json_array]);
+        echo json_encode($json_array);
 ?>
               
-
